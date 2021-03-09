7f0bfa29d10e550322f6192e38ecc7c73605ec36
ItsOnlyGame <heinonenjaro@gmail.com>
2021-03-09 11:11:46 +0200

    Fixed meme generator

--- Webpages/meme-generator/handler.js
+++ Webpages/meme-generator/handler.js
@@ -1,3 +1,5 @@
+const meme_queue = [];
+
 async function execute() {
     const loading_text = document.getElementById("pic-loading");
     const info_text = document.getElementById("pic-info");
@@ -6,20 +8,36 @@ async function execute() {
     image.src = "";
     loading_text.innerHTML = "Loading...";
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    };
-    const response = await fetch('/api/meme', options)
-    const json = await response.json();
+    if (meme_queue.length < 5) {
+        var promise = new Promise(async(resolve) => {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            };
+            const response = await fetch('/api/meme', options)
+            const json = await response.json();
+                
+            json.memes.forEach(meme => {
+                meme_queue.push(meme);
+            });
+            resolve();
+        });
+    }
 
 
-    image.src = json.image_url;
+    if (meme_queue.length == 0) {
+        await promise;
+    }
+
+    const meme = meme_queue.pop();
+
+    image.src = meme.image_url;
     loading_text.innerHTML = "";
-    info_text.innerHTML = json.reddit_url;
-    info_text.href = json.reddit_url;
+
+    info_text.innerHTML = meme.reddit_url;
+    info_text.href = meme.reddit_url;
 }
 
 
