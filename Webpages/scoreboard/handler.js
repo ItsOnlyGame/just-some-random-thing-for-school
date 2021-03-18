463273c40ca8c57ab26cab7ff02ca6a7ba8a30af
ItsOnlyGame <heinonenjaro@gmail.com>
2021-03-18 09:51:20 +0200

    Propably the last commit to this project ;(

--- Webpages/scoreboard/handler.js
+++ Webpages/scoreboard/handler.js
@@ -15,27 +15,25 @@ async function createTableContent() {
     const memes = data.data;
     const table = document.getElementById("scoreboard");
 
-
-
-
     for (const meme of memes) {
         var row = table.insertRow();
 
         createCellToTable(meme.name, row);
-        createCellToTable(meme.redditUrl, row, true);
+        createCellToTable(meme.redditUrl, row, {enabled: true, link: meme.redditUrl});
         createCellToTable(meme.memeId.split(":")[0], row);
         createCellToTable(meme.score, row);
+        createCellToTable("Click me", row, {enabled: true, link: meme.imageUrl});
     }
 
 }
 
-function createCellToTable(key, row, link = false) {
+function createCellToTable(key, row, href = {enabled: false, link: "", target: "_blank"}) {
     let cell = row.insertCell();
 
-    if (link) {
+    if (href.enabled) {
         var a = document.createElement("a");
-        a.href = key;
-        a.target = "_blank";
+        a.href = href.link;
+        a.target = href.target;
         a.innerHTML = key;
         a.style = "color: white";
         cell.append(a)
